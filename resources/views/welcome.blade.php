5c8245e8123fdff5d5ac579eac39d4d485f96f39
Mohammed Al-Haj <w3ghost775@gmail.com>
2024-04-29 11:35:46 +0300

    Add moha

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,6 +22,7 @@
     </head>
     <body class="antialiased">
         <h2>Bilal</h2>
+        <h3>Moha</h3>
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             
             @if (Route::has('login'))
