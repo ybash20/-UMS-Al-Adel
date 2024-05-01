f1106154cccae421dc8b743385aefdc455118b31
Bilal Hasil <belal66ali70@gmail.com>
2024-05-01 11:48:10 +0300

    Library

--- resources/views/library.blade.php
+++ resources/views/library.blade.php
@@ -115,70 +115,157 @@
 
 <link rel="stylesheet" href="css/lib/stylelib.css">
 
-
-{{-- <div class="main">
-
-    <div class="main_tag">
-        <h1>WELCOME TO<br><span>BOOK STORE</span></h1>
-
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda molestias atque laborum 
-            non fuga ex deserunt. Exercitationem velit ducimus praesentium, obcaecati hic voluptate id 
-            tenetur fuga illum quidem omnis? Rerum?
-        </p>
-        <a href="#" class="main_btn">Learn More</a>
-
-    </div>
-
-    <div class="main_img">
-        <img src="image/table.png">
-    </div>
-
-</div> --}}
-
 <!--About-->
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
 
-{{-- <div class="about">
 
-<div class="about_image">
-    <img src="image/about.png">
-</div>
+<div class="about">
+
 <div class="about_tag">
     <h1>About Us</h1>
-    <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae cumque atque dolor corporis 
-        architecto. Voluptate expedita molestias maxime officia natus consectetur dolor quisquam illo? 
-        Quis illum nostrum perspiciatis laboriosam perferendis? Lorem ipsum dolor sit amet consectetur 
-        adipisicing elit. Minus ad eius saepe architecto aperiam laboriosam voluptas nobis voluptates 
-        id amet eos repellat corrupti harum consectetur, dolorum dolore blanditiis quam quo.
+    <p style="font-size: 20px">
+        There is a library equipped with reference books and amenities to achieve its goal. The library contains more than 1,800 titles in various sciences, such as: Qur’anic sciences, hadith, jurisprudence, principles, translations, history, languages, translation, economics, management, computer science, and information technology. This is in addition to a number of educational publications, cultural periodicals, peer-reviewed journals, and scientific theses. Internet service and e-books are also available in the electronic resource room.
+Here on the website we provide the names of books and authors available in the library
     </p>
-    <a href="#" class="about_btn">Learn More</a>
 </div>
 
-</div> --}}
-
-
+</div>
+<hr style="background-color:  black;">
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
 <!--Arrivals-->
 
 <div class="arrivals">
-<h1>New Arrivals</h1>
-
+<h1 style="text-align: center">Book</h1>
+<h2 style="text-align: center; direction: rtl">
+<form action="">
+    <label for="categories">اختر القسم:</label>
+    <BR></BR>
+    <select name="categories" id="categories">
+      <option value="S1">الحاسوب</option>
+      <option value="S2">الترجمة</option>
+      <option value="S3"> أدارة الأعمال</option>
+      <option value="S4">قانون</option>
+    </select>
+    <input type="submit" value="تحديد">
+  </form>
+</h2>
+<hr style="background-color:  black;">
+<br>
+<br>
 <div class="arrivals_box">
+    <div class="arrivals_card">
+        <div class="arrivals_image">
+            <img src="image/arrival_2.jpg">
+        </div>
+        <div class="arrivals_tag"> 
+            <p>
+               <h2>book name:شستنىشستنشسىتنشسىتنشسىتنشسىشستن</h2>
+                <p>author:شسمةشسنمةشسنمشسةنمشسةنمشسةمنشسةشسم</p>
+                <p>publiser:ششسمنةسنمشسةنمسةشسنمة</p>
+            </p>
+        
+        </div>
+    </div>
 
     <div class="arrivals_card">
         <div class="arrivals_image">
-            <img src="image/arrival_1.jpg">
+            <img src="image/arrival_2.jpg">
         </div>
-        <div class="arrivals_tag">
-            <p>New Arrivals</p>
-            <div class="arrivals_icon">
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star"></i>
-                <i class="fa-solid fa-star-half-stroke"></i>
-            </div>
-            <a href="#" class="arrivals_btn">Learn More</a>
+        <div class="arrivals_tag"> 
+            <p>
+               <h2>book name:</h2>
+                <p>author:</p>
+                <p>publiser:</p>
+            </p>
+        
+        </div>
+    </div>
+    <div class="arrivals_card">
+        <div class="arrivals_image">
+            <img src="image/arrival_2.jpg">
+        </div>
+        <div class="arrivals_tag"> 
+            <p>
+               <h2>book name:شنتىسشتنىسشتنشىشستنىشستنسىتنشسىستنىشسنتىشسنتسىتنىشستن</h2>
+                <p>author:سششسنمةشسنمشسةنمشسةشسنمةشسنمةشسنمشسةمنشسةنمشسة</p>
+                <p>publiser:شسمنةشسنمشسةمنشسةنمش</p>
+            </p>
+        
+        </div>
+    </div>
+    <div class="arrivals_card">
+        <div class="arrivals_image">
+            <img src="image/arrival_2.jpg">
+        </div>
+        <div class="arrivals_tag"> 
+            <p>
+               <h2>book name:</h2>
+                <p>author:</p>
+                <p>publiser:</p>
+            </p>
+        
+        </div>
+    </div>
+    <div class="arrivals_card">
+        <div class="arrivals_image">
+            <img src="image/arrival_2.jpg">
+        </div>
+        <div class="arrivals_tag"> 
+            <p>
+               <h2>book name:</h2>
+                <p>author:</p>
+                <p>publiser:</p>
+            </p>
+        
+        </div>
+    </div>
+    <div class="arrivals_card">
+        <div class="arrivals_image">
+            <img src="image/arrival_2.jpg">
+        </div>
+        <div class="arrivals_tag"> 
+            <p>
+               <h2>book name:</h2>
+                <p>author:</p>
+                <p>publiser:</p>
+            </p>
+        
+        </div>
+    </div>
+    <div class="arrivals_card">
+        <div class="arrivals_image">
+            <img src="image/arrival_2.jpg">
+        </div>
+        <div class="arrivals_tag"> 
+            <p>
+               <h2>book name:</h2>
+                <p>author:</p>
+                <p>publiser:</p>
+            </p>
+        
+        </div>
+    </div>
+    <div class="arrivals_card">
+        <div class="arrivals_image">
+            <img src="image/arrival_2.jpg">
+        </div>
+        <div class="arrivals_tag"> 
+            <p>
+               <h2>book name:ashbasbasjhasajhbajhsbasjhb</h2>
+                <p>author:asjknasjknasjkasnjkasnasjkn</p>
+                <p>publiser:asklmasklmsklsmklsamlaskmlaskm</p>
+            </p>
+        
         </div>
     </div>
 </div>
