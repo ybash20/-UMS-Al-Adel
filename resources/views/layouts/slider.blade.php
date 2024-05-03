0d3f211f076dcf7dc3b0fb3198d05b11e6445fa8
Ayman <aymanabuali@hotmail.com>
2024-05-03 18:42:20 +0300

    add slider as layout

---
+++ resources/views/layouts/slider.blade.php
@@ -0,0 +1,72 @@
+@section('slider')
+<link rel="stylesheet" href="css/lib/styleslider.css">
+<div class="center">
+    <div class="body_slider">
+        <div class="slider">
+            <!-- list Items -->
+            <div class="list">
+                <div class="item active">
+                    <img src="image/img1.png">
+                    <div class="content">
+                        <p>design</p>
+                        <h2>Slider 01</h2>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, neque?
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, ex.
+                        </p>
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="image/img2.jpg">
+                    <div class="content">
+                        <p>design</p>
+                        <h2>Slider 02</h2>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, neque?
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, ex.
+                        </p>
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="image/img3.jpg">
+                    <div class="content">
+                        <p>design</p>
+                        <h2>Slider 03</h2>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, neque?
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, ex.
+                        </p>
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="image/img4.jpg">
+                    <div class="content">
+                        <p>design</p>
+                        <h2>Slider 04</h2>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, neque?
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, ex.
+                        </p>
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="image/img5.jpg">
+                    <div class="content">
+                        <p>design</p>
+                        <h2>Slider 05</h2>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, neque?
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, ex.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <!-- button arrows -->
+            <div class="arrows">
+                <button id="prev"><</button>
+                <button id="next">></button>
+            </div>
+        </div>
+    </div>
+    <script src="js/lib/js_slider_library.js"></script>
+@show
--- resources/views/library.blade.php
+++ resources/views/library.blade.php
@@ -2,78 +2,8 @@
 @section('title', 'Library')
 <!-- slider -->
 @section('divcontent')
-    <link rel="stylesheet" href="css/lib/styleslider.css">
-    <div class="center">
-        <div class="body_slider">
-            <div class="slider">
-                <!-- list Items -->
-                <div class="list">
-                    <div class="item active">
-                        <img src="image/img1.png">
-                        <div class="content">
-                            <p>design</p>
-                            <h2>Slider 01</h2>
-                            <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, neque?
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, ex.
-                            </p>
-                        </div>
-                    </div>
-                    <div class="item">
-                        <img src="image/img2.jpg">
-                        <div class="content">
-                            <p>design</p>
-                            <h2>Slider 02</h2>
-                            <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, neque?
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, ex.
-                            </p>
-                        </div>
-                    </div>
-                    <div class="item">
-                        <img src="image/img3.jpg">
-                        <div class="content">
-                            <p>design</p>
-                            <h2>Slider 03</h2>
-                            <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, neque?
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, ex.
-                            </p>
-                        </div>
-                    </div>
-                    <div class="item">
-                        <img src="image/img4.jpg">
-                        <div class="content">
-                            <p>design</p>
-                            <h2>Slider 04</h2>
-                            <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, neque?
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, ex.
-                            </p>
-                        </div>
-                    </div>
-                    <div class="item">
-                        <img src="image/img5.jpg">
-                        <div class="content">
-                            <p>design</p>
-                            <h2>Slider 05</h2>
-                            <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, neque?
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, ex.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <!-- button arrows -->
-                <div class="arrows">
-                    <button id="prev"><</button>
-                    <button id="next">></button>
-                </div>
-            </div>
-        </div>
-        <script src="js/lib/js_slider_library.js"></script>
-
-    @endsection
+    @include('layouts.slider')
+@endsection
 
     @section('main')
     <link rel="stylesheet" href="css/lib/stylelib.css">
