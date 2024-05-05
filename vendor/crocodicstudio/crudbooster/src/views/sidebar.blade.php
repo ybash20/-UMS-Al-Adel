3e7ffade5c9300489669fbf3db8254526c92c318
Ayman <aymanabuali@hotmail.com>
2024-05-05 21:18:37 +0300

    fix language bug

--- vendor/crocodicstudio/crudbooster/src/views/sidebar.blade.php
+++ vendor/crocodicstudio/crudbooster/src/views/sidebar.blade.php
@@ -15,15 +15,22 @@
                 <a href="#"><i class="fa fa-circle text-success"></i> {{ cbLang('online') }}</a>
             </div>
         </div>
-
         <div class='main-menu'>
             <ul class="sidebar-menu">
                 <li class="header">{{ cbLang('menu_lang') }}</li>
-                 <form id="langForm" method="get" action="" class="form_select">
-                    <button name="lang" value="ar" <?php if ($language == 'ar') { echo 'selected'; } ?>>عربي</button>
-                    <button name="lang" value="en" <?php if ($language == 'en') { echo 'selected'; } ?>>English</button>
+                 <form id="langForm" method="get" action="<?php htmlspecialchars($_SERVER["PHP_SELF"]); ?>" class="form_select">
+                    <button type="submit" name="lang" value="ar">عربي</button>
+                    <button type="submit" name="lang" value="en">English</button>
                     <!-- يمكنك إضافة المزيد من الخيارات حسب الحاجة -->
                 </form>
+                <?php
+                if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+                    if (isset($_GET['lang'])) {
+                        $lang = $_GET['lang'];
+                        setcookie('lang', $lang, time() + 3 * 24 * 60 * 60); // Set cookie for 3 days
+                    }
+                }
+                ?>
                 <!-- Sidebar Menu -->
                 <li class="header">{{ cbLang('menu_navigation') }}</li>
                 <!-- Optionally, you can add icons to the links -->
