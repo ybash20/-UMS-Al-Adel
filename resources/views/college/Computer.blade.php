18cb8213cd1af1087823c5365e78a673712ae5d7
Mohammed Al-Haj <w3ghost775@gmail.com>
2024-05-16 21:14:00 +0300

    edit computer

--- resources/views/college/Computer.blade.php
+++ resources/views/college/Computer.blade.php
@@ -4,11 +4,9 @@
 
 @section('main') --}}
 <style>
-    @import url('https://fonts.googleapis.com/css?family=lemonad:500,500,300,600,700,800,900,500&display=swap');
+    @import url('https://fonts.googleapis.com/css?family=Cairo:200,300,400,500,600,700,800,900&display=swap');
 
-
-
-    .body_computer {
+    body {
         background: #FFF;
         text-align: center;
         color: #212529;
@@ -23,7 +21,7 @@
         box-sizing: border-box;
     }
 
-    .row-lzg {
+    .row-nvg {
         display: flex;
         flex-wrap: wrap;
         margin-right: -15px;
@@ -38,7 +36,7 @@
         }
     }
 
-    .as_body {
+    body {
         background-color: #fff;
         color: #212529;
         font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
@@ -50,7 +48,7 @@
         text-align: inherit;
     }
 
-    .oo_body {
+    body {
         background-color: #FFF;
         color: #777;
         font-family: 'Cairo', sans-serif;
@@ -60,12 +58,12 @@
         text-align: right;
     }
 
-    .re_html {
+    html {
         -webkit-text-size-adjust: 100%;
         -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
     }
 
-    .rt_html {
+    html {
         direction: rtl;
         overflow-x: hidden;
         box-shadow: none !important;
@@ -78,11 +76,11 @@
         }
     }
 
-    .p-9o8 {
+    .p-cby {
         padding: 1.5rem !important;
     }
 
-    .content-7d7 {
+    .content-q4i {
         border-radius: 8px;
         border-top: 1px solid rgba(0, 0, 0, 0.06);
         border-top-width: 4px;
@@ -90,11 +88,11 @@
         position: relative;
     }
 
-    .content-7d7 {
+    .content-q4i {
         border-top-color: #0088CC;
     }
 
-    .content-7d7:not(.box-content-border-0) {
+    .content-q4i:not(.box-content-border-0) {
         top: -1px;
         border-top-width: 4px;
     }
@@ -111,72 +109,72 @@
     }
 
     @media (min-width: 1200px) {
-        .item-rii {
+        .item-dr7 {
             align-items: center !important;
         }
     }
 
-    .py-gy9 {
+    .py-r1z {
         padding-top: 1.5rem !important;
     }
 
-    .py-gy9 {
+    .py-r1z {
         padding-bottom: 1.5rem !important;
     }
 
-    .col-cyc {
+    .col-rfy {
         padding-right: 15px;
         padding-left: 15px;
         position: relative;
         width: 100%;
     }
 
-    .col-cyc {
+    .col-rfy {
         flex: 0 0 25%;
         max-width: 25%;
     }
 
-    .col-6l9 {
+    .col-qxc {
         padding-right: 15px;
         padding-left: 15px;
         position: relative;
         width: 100%;
     }
 
-    .col-6l9 {
+    .col-qxc {
         flex: 0 0 75%;
         max-width: 75%;
     }
 
-    .col-mdz {
+    .col-wli {
         padding-right: 15px;
         padding-left: 15px;
         position: relative;
         width: 100%;
     }
 
-    .col-mdz {
+    .col-wli {
         flex-basis: 0;
         flex-grow: 1;
         max-width: 100%;
     }
 
-    .s_h2 {
+    h2 {
         margin-bottom: .5rem;
         margin-top: 0;
     }
 
-    .t_h2 {
+    h2 {
         font-weight: 500;
         line-height: 1.2;
         margin-bottom: .5rem;
     }
 
-    .h_h2 {
+    h2 {
         font-size: 2rem;
     }
 
-    .y_h2 {
+    h2 {
         color: #212529;
         font-weight: 200;
         letter-spacing: -.05em;
@@ -184,29 +182,29 @@
         -webkit-font-smoothing: antialiased;
     }
 
-    .jh2 {
+    h2 {
         font-size: 2.2em;
         font-weight: 300;
         line-height: 42px;
         margin: 0 0 32px 0;
     }
 
-    .font-weight-io5 {
+    .font-weight-jqf {
         font-weight: 400 !important;
     }
 
-    .text-c8a {
+    .text-ynx {
         font-size: 1.80em !important;
     }
 
-    .col-6l9 h2 {
+    .col-qxc h2 {
         text-align: right;
         margin-bottom: 0;
         position: relative;
     }
 
-    .featured-box.featured-box-primary.featured-box-text-start.mt-0 .col-6l9 h2::after,
-    .featured-box.featured-box-primary.featured-box-text-start.mt-0 .col-mdz h2::after {
+    .featured-box.featured-box-primary.featured-box-text-start.mt-0 .col-qxc h2::after,
+    .featured-box.featured-box-primary.featured-box-text-start.mt-0 .col-wli h2::after {
         content: "";
         position: absolute;
         bottom: -14px;
@@ -232,7 +230,7 @@
         text-align: justify;
     }
 
-    .text-ar3 {
+    .text-6il {
         font-size: 1.1em !important;
     }
 
@@ -240,7 +238,7 @@
         vertical-align: middle;
     }
 
-    .leader-zde img {
+    .leader-olg img {
         width: 110px;
         height: 110px;
         border-radius: 50%;
@@ -250,7 +248,7 @@
         font-weight: bolder;
     }
 
-    .font-weight-extra-84k {
+    .font-weight-extra-jqe {
         font-weight: 800 !important;
     }
 
@@ -281,7 +279,7 @@
         -webkit-appearance: button;
     }
 
-    .btn-p8r {
+    .btn-y2x {
         background-color: transparent;
         border: 1px solid transparent;
         border-radius: .25rem;
@@ -298,7 +296,7 @@
         vertical-align: middle;
     }
 
-    .box-dgd {
+    .box-iyc {
         color: #c79d2d;
         border: 1px solid;
         border-radius: 30px;
@@ -306,151 +304,14 @@
         font-weight: 500;
     }
 
-    .btn-p8r {
+    .btn-y2x {
         font-size: 0.8rem;
         padding: 0.533rem 0.933rem;
         cursor: pointer;
     }
 
-    .scoog {
+    .ovsoj {
         float: left;
-    }
-
-    a:hover {
-        color: #0056b3;
-        text-decoration: underline;
-    }
-
-    .ahover {
-        color: #0099e6;
-    }
-
-
-    .a_hover {
-        color: #1b3c6a;
-    }
-
-    .btn-p8r:hover {
-        color: #212529;
-        text-decoration: none;
-    }
-
-    .box-dgd:hover {
-        color: #fff;
-        background: #c79d2d;
-    }
-
-    @import url('https://fonts.googleapis.com/css?family=Cairo:200,300,400,500,600,700,800,900&display=swap');
-
-    body {
-        color: #212529;
-        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
-        font-size: 1rem;
-        font-weight: 400;
-        line-height: 1.5;
-        text-align: inherit;
-        direction: rtl;
-    }
-
-    * {
-        box-sizing: border-box;
-    }
-
-    @media (min-width: 1200px) {
-        .container:not(.container-bs):not(.container-xl-custom):not(.container-xxl-custom) {
-            max-width: 1170px;
-            margin: 0 auto;
-            padding: 0 24px;
-        }
-    }
-
-    body {
-        background-color: #fff;
-        color: #212529;
-        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
-        font-size: 1rem;
-        font-weight: 400;
-        line-height: 1.5;
-        margin: 0;
-        -ms-overflow-style: scrollbar;
-        text-align: inherit;
-    }
-
-    body {
-        background-color: #FFF;
-        color: #777;
-        font-family: 'Cairo', sans-serif;
-        font-size: 14px;
-        line-height: 26px;
-        margin: 0;
-        text-align: right;
-    }
-
-    html {
-        -webkit-text-size-adjust: 100%;
-        -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
-    }
-
-    html {
-        direction: rtl;
-        overflow-x: hidden;
-        box-shadow: none !important;
-        -webkit-font-smoothing: antialiased;
-    }
-
-    @media (prefers-reduced-motion: no-preference) {
-        :root {
-            scroll-behavior: auto;
-        }
-    }
-
-    *,
-    :before,
-    :after {
-        box-sizing: border-box;
-    }
-
-    :selection {
-        color: #FFF;
-        background: #0088CC;
-    }
-
-    .content-mym:not(.box-content-border-0) {
-        top: -1px;
-        border-top-width: 4px;
-    }
-
-    .box-hb3.box-p66.text-jt4.mt-8dk .col-9 h2::after,
-    .box-hb3.box-p66.text-jt4.mt-8dk .col-oo4 h2::after {
-        content: "";
-        position: absolute;
-        bottom: -14px;
-        right: 0;
-        height: 4px;
-        background: #c99e2d;
-        width: 60px;
-    }
-
-    a {
-        color: #007bff;
-        cursor: pointer;
-        text-decoration: none;
-    }
-
-    a {
-        color: #c99e2d;
-    }
-
-    a {
-        text-decoration: none;
-    }
-
-    a {
-        color: #1b3c6a;
-    }
-
-    a {
-        outline: none !important;
     }
 
     a:hover {
@@ -467,97 +328,48 @@
         color: #1b3c6a;
     }
 
-    .btn-kra:hover {
+    .btn-y2x:hover {
         color: #212529;
         text-decoration: none;
     }
 
-    .box-pnr:hover {
+    .box-iyc:hover {
         color: #fff;
         background: #c79d2d;
-    }
-
-
-    /* These were inline style tags. Uses id+class to override almost everything */
-    #style-67UOo.style-67UOo {
-        box-sizing: border-box;
-        border-style: solid;
-        border-width: 0px;
-        opacity: 1.0;
     }
 </style>
 
 
 
 
-<div class="content-7d7 p-9o8">
-    <div class="row-lzg item-rii">
-        <div class="col-cyc">
-            <div class="leader-zde">
+<div class="content-q4i p-cby">
+    <div class="row-nvg item-dr7">
+        <div class="col-rfy">
+            <div class="leader-olg">
                 <img
                     src="https://cscis.nu.edu.sa/documents/46874/0/WhatsApp+Image+2022-04-19+at+6.24.19+AM+%281%29.jpeg/6d378670-d6e8-1cc7-d03d-1204199a851b?t=1682237823865&amp;download=true">
             </div>
         </div>
-        <div class="col-6l9">
-            <h2 class="font-weight-io5 text-c8a">كلمة العميد&nbsp;<br>
-                <strong class="font-weight-extra-84k">د.خالد بانافع </strong>
-            </h2>
-        </div>
-    </div>
-    <div class="row-lzg py-gy9">
-        <div class="col-mdz">
-            <p class="text-ar3">
-                زوارنا الكرام .. نرحب بكم في موقع كلية علوم الحاسوب وتقنية المعلومات ولا يسعنا هنا الا اعطاءكم نبذة
-                صغيرة عن الكلية.
-                أنشئت كلية علوم الـحاسوب وتقنية المعلومات في جامعة العادل لمد المجتمع بالكوادر المؤهلة والمواكبة
-                للتطورات الحاصلة في حقل علوم الحاسوب وتقنية المعلومات التي أصبحت واحدة من الصفات المميزة للتعليم في
-                القرن الواحد والعشرين في تلك العلوم مهتدين برؤية ورسالة الجامعة وسعيا لتحقيق اهداف الجامعة مما يساهم في
-                دفع عجلة الـــتنمية الى الأمام من خــلال إعطاء الطــــالب الــمهارات اللازمة ليصبح عضواً نافعاُ وإنساناً
-                صالحاً لــدينه ووطنه ونفسه.
-                ومن اجل قيام الكلية بدورها في تحقيق أهدافها قامت الكلية بتوفير المتطلبات اللازمة للتعليم والتعلّم والبحث
-                العلمي من خلال استقطاب كوادر تدريسية متميزة وتجهيز بنية تحتية متميزة تشمل قاعات حديثة ومختبرات متقدمة
-                ومتخصصة بشاشات ذكية وعدد كبير من أجهزة الحاسوب المتقدمة وملحقاتها ومكتبة ورقية ورقمية.
-                تسعى الكلية إلى الاندماج في سوق العمل من خلال دراسة متطلبات السوق وانعكاس ذلك في الخطط الدراسية
-                واستراتيجيات التعليم والتعلم، كما وتسعى الكلية إلى إقامة شراكات مع جامعات محلية وعالمية من خلال مذكرات
-                التفاهم والتوأمة. كما تسعى الكلية جاهدة إلى تطبيق أفضل معايير الجودة والاعتماد المؤسسي والبرامجي المحلي
-                والعالمي لضمان حصول الخريج على الكفايات المطلوبة لمواكبة تطورات هذا العصر.
-
-
-
-        </div>
-    </div>
-    
-</div>
-<div style="padding:5px">
-    <div>
-        <div>
-            <div class="style-67UOo" id="style-67UOo">
-                <div>
-                    <div>
-                        <div class="box-hb3 box-p66 text-jt4 mt-8dk">
-                            <div class="content-mym">
-                                <div>
-                                    <div class="col-oo4">
-                                        <div>
-                                            {{-- <img
-                                                src="https://cscis.nu.edu.sa/documents/46874/0/208619947477+%281%29.jpg/e5fc90ae-105c-1501-f759-80bd43dde305?t=1652174393596&amp;download=true"> --}}
-                                        </div>
-                                    </div>
-                                </div>
-                                <div>
-                                    <div class="col-oo4">
-                                        <h2>نبذه عن <b>الكلية</b></h2>
-                                        <p>أُنشئت كلية علوم الحاسب ونظم المعلومات بجامعة نجران في مطلع العام الجامعي
-                                            1427 هـ حيث شكل قسمي علوم الحاسب و نظم المعلومات النواة الأولى للكلية مقدمةً
-                                            من خلالهما عدد من البرامج في مجال الحاسوب وتقنية المعلومات لصناعة السوق
-                                            المحلية ولمواكبة التطورات التقنية وتحقيق متطلبات التعليم العالي في وطننا
-                                            الغالي.</p>
-
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+        <div class="col-qxc">
+       
+   
+        <div class="box-content p-4 XXsnipcss_extracted_selector_selectionXX snipcss0-0-0-1 tether-element-attached-top tether-element-attached-center tether-target-attached-top tether-target-attached-center">
+            <div class="row align-items-xl-center snipcss0-1-1-2">
+                <div class="col-3 snipcss0-2-2-3">
+                    <div class="text-end leader-img snipcss0-3-3-4">
+                        <img src="https://cscis.nu.edu.sa/documents/46874/0/WhatsApp+Image+2022-04-19+at+6.24.19+AM+%281%29.jpeg/6d378670-d6e8-1cc7-d03d-1204199a851b?t=1682237823865&amp;download=true" alt="" title="" data-lfr-editable-id="card1" data-lfr-editable-type="image" class="snipcss0-4-4-5">
                     </div>
+                </div>
+                <div class="col-9 snipcss0-2-2-6">
+                    <h2 class="font-weight-normal text-6 snipcss0-3-6-7" data-lfr-editable-id="card2" data-lfr-editable-type="rich-text">كلمة العميد&nbsp;<br class="snipcss0-4-7-8">
+    <strong class="font-weight-extra-bold snipcss0-4-7-9">د. محمد علي حمدي</strong></h2>
+                </div>
+            </div>
+            <div class="row py-4 text-end snipcss0-1-1-10">
+                <div class="col snipcss0-2-10-11">
+                    <p class="text-3-4 snipcss0-3-11-12" data-lfr-editable-id="card3" data-lfr-editable-type="rich-text">الحمد لله وحده والصلاة والسلام على من لا نبي بعده وعلى آله وصحبه<br class="snipcss0-4-12-13">
+    أصبح الناس على مختلف مستوياتهم يدركون أهمية الحاسوب وتقنية المعلومات ويشعرون بالقفزة الحضارية الكبيرة التي أدخلها الحاسوب على العالم. ففي غضون سنوات قليلة دخلت الإنترنت إلى وعي كثير من الناس وصارت هذه الشبكة المتصلة بمئات الملايين من الحواسيب عبر العالم وسيلة للتواصل ونقل المعلومات بين أطراف العالم المترامية بحرية وبسرعة فائقة. وبلا ريب أن هذه المعلومات المتداولة تحتوي مضامين مختلفة تشكل ثقافة الإنسان وتدير دفة حضارة المجتمعات. ففي ضوء هذا الادراك لأهمية الحاسوب وتقنية المعلومات فقد حرصت جامعة نجران منذ نشأتها على إطلاق برامج نوعية في مجالات التقنية باستحداث كلية علوم الحاسب ونظم المعلومات.</p>
+                     <div class="snipcss0-3-11-14"><a href="https://cscis.nu.edu.sa/ar/dean-message" class="btn btn-box flo snipcss0-4-14-15" type="button">المزيد</a></div>
                 </div>
             </div>
         </div>
