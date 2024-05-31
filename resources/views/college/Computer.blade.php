7183ebe465bfb1731b5b57b062709418ef5c6762
Mohammed Al-Haj <w3ghost775@gmail.com>
2024-05-31 21:13:08 +0300

    edit computer

--- resources/views/college/Computer.blade.php
+++ resources/views/college/Computer.blade.php
@@ -4,17 +4,6 @@
 
 @section('main')
     <style>
-        .body_computer {
-            background: #FFF;
-            text-align: center;
-            color: #212529;
-            font-family: 'Lemonada', cursive;
-            font-size: 1rem;
-            font-weight: 400;
-            line-height: 1.5;
-            direction: rtl;
-        }
-
         * {
             box-sizing: border-box;
 
@@ -23,50 +12,9 @@
         .row-nvg {
             text-align: right;
             /* margin-right: -24px;
-            margin-left: -30px; */
-            font-size: 15px;
+                            margin-left: -30px; */
+            font-size: 20px;
 
-        }
-
-        @media (min-width: 1200px) {
-            .container:not(.container-bs):not(.container-xl-custom):not(.container-xxl-custom) {
-                max-width: 1170px;
-                margin: 0 auto;
-                padding: 0 24px;
-            }
-        }
-
-        .body_computer1 {
-            background-color: #fff;
-            color: #212529;
-            font-size: 1rem;
-            font-weight: 400;
-            line-height: 1.5;
-            margin: 0;
-            -ms-overflow-style: scrollbar;
-            text-align: inherit;
-        }
-
-        .body_computer2 {
-            background-color: #FFF;
-            color: #777;
-            font-family: 'Cairo', sans-serif;
-            font-size: 14px;
-            line-height: 26px;
-            margin: 0;
-            text-align: right;
-        }
-
-        .html_computer {
-            -webkit-text-size-adjust: 100%;
-            -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
-        }
-
-        .html_computer1 {
-            direction: rtl;
-            overflow-x: hidden;
-            box-shadow: none !important;
-            -webkit-font-smoothing: antialiased;
         }
 
         @media (prefers-reduced-motion: no-preference) {
@@ -89,9 +37,8 @@
             padding: 15px;
             /* border-left: groove; */
             /* border-right: groove; */
-            box-shadow: 6px 7px 19px -7px;
+            box-shadow: 9px 8px 13px -9px #777;
         }
-        
 
         .content-q4i {
             border-top-color: #0088CC;
@@ -122,26 +69,6 @@
             }
         }
 
-        .py-r1z {
-            padding-top: 1.5rem !important;
-        }
-
-        .py-r1z {
-            padding-bottom: 1.5rem !important;
-        }
-
-        .col-rfy {
-            padding-right: 15px;
-            padding-left: 15px;
-            position: relative;
-            width: 100%;
-        }
-
-        .col-rfy {
-            flex: 0 0 25%;
-            max-width: 25%;
-        }
-
         .col-qxc {
             padding-right: 15px;
             padding-left: 15px;
@@ -153,190 +80,13 @@
             flex: 0 0 75%;
         }
 
-        .col-wli {
-            padding-right: 15px;
-            padding-left: 15px;
-            position: relative;
-            width: 100%;
-        }
-
-        .col-wli {
-            flex-basis: 0;
-            flex-grow: 1;
-            max-width: 100%;
-        }
-
-        .h2_1 {
-            margin-bottom: .5rem;
-            margin-top: 0;
-        }
-
-        .h2_2 {
-            font-weight: 500;
-            line-height: 1.2;
-            margin-bottom: .5rem;
-        }
-
-        .h2_3 {
-            font-size: 2rem;
-        }
-
-        .h2_4 {
-            color: #212529;
-            font-weight: 200;
-            letter-spacing: -.05em;
-            margin: 0;
-            -webkit-font-smoothing: antialiased;
-        }
-
-        .h2_5 {
-            font-size: 2.2em;
-            font-weight: 300;
-            line-height: 42px;
-            margin: 0 0 32px 0;
-        }
-
-        .font-weight-jqf {
-            font-weight: 400 !important;
-        }
-
-        .text-ynx {
-            font-size: 1.80em !important;
-        }
-
-        .content-q4i p-cby {
-            margin-top: 35px;
-        }
-
         .col-qxc h2 {
             text-align: right;
             margin-bottom: 0;
             position: relative;
         }
 
-
-        .featured-box.featured-box-primary.featured-box-text-start.mt-0 .col-qxc h2::after,
-        .featured-box.featured-box-primary.featured-box-text-start.mt-0 .col-wli h2::after {
-            content: "";
-            position: absolute;
-            bottom: -14px;
-            right: 0;
-            height: 4px;
-            background: #c99e2d;
-            width: 60px;
-        }
-
-        .p_1 {
-            margin-bottom: 1rem;
-            margin-top: 0;
-        }
-
-        .p_2 {
-            text-align: justify !important;
-        }
-
-        .p_3 {
-            color: #212529;
-            line-height: 26px;
-            margin: 0 0 20px;
-            text-align: justify;
-        }
-
-        .text-6il {
-            font-size: 1.1em !important;
-        }
-
-        .img_1 {
-            vertical-align: middle;
-        }
-
-        .leader-olg img {
-            width: 110px;
-            height: 110px;
-            border-radius: 50%;
-        }
-
-        .strong_1 {
-            font-weight: bolder;
-        }
-
-        .font-weight-extra-jqe {
-            font-weight: 800 !important;
-        }
-
-        .a_1 {
-            color: #007bff;
-            cursor: pointer;
-            text-decoration: none;
-        }
-
-        .a_2 {
-            color: #c99e2d;
-        }
-
-        .a_3 {
-            text-decoration: none;
-        }
-
-        .a_4 {
-            color: #1b3c6a;
-        }
-
-        .a_5 {
-            outline: none !important;
-        }
-
-        [type="button"] {
-            cursor: pointer;
-            -webkit-appearance: button;
-        }
-
-        .btn-y2x {
-            background-color: transparent;
-            border: 1px solid transparent;
-            border-radius: .25rem;
-            color: #212529;
-            cursor: pointer;
-            display: inline-block;
-            font-size: 1rem;
-            font-weight: 400;
-            line-height: 1.5;
-            padding: .375rem .75rem;
-            text-align: center;
-            transition: color .15s ease-in-out, background-color .15s ease-in-out, border-color .15s ease-in-out, box-shadow .15s ease-in-out;
-            user-select: none;
-            vertical-align: middle;
-        }
-
-        .box-iyc {
-            color: #c79d2d;
-            border: 1px solid;
-            border-radius: 30px;
-            font-size: 13px;
-            font-weight: 500;
-        }
-
-        .btn-y2x {
-            font-size: 0.8rem;
-            padding: 0.533rem 0.933rem;
-            cursor: pointer;
-        }
-
-        .ovsoj {
-            float: left;
-        }
-
-        .a:hover {
-            color: #0056b3;
-            text-decoration: underline;
-        }
-
-        .a:hover {
-            color: #0099e6;
-        }
-
-        a,
-        .a:hover {
+        a {
             color: #1b3c6a;
         }
 
@@ -350,14 +100,273 @@
             }
         }
 
-        .btn-y2x:hover {
+        /*iiiii*/
+
+        /* @import url('https://fonts.googleapis.com/css?family=Cairo:200,300,400,500,600,700,800,900&display=swap'); */
+
+        .body {
             color: #212529;
+            font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+            font-size: 1rem;
+            font-weight: 400;
+            line-height: 1.5;
+            text-align: inherit;
+            direction: rtl;
+        }
+
+        * {
+            box-sizing: border-box;
+        }
+
+        .body {
+            background-color: #fff;
+            color: #212529;
+            font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+            font-size: 1rem;
+            font-weight: 400;
+            line-height: 1.5;
+            margin: 0;
+            -ms-overflow-style: scrollbar;
+            text-align: inherit;
+        }
+
+        .body {
+            background-color: #FFF;
+            color: #777;
+            font-family: 'Cairo', sans-serif;
+            font-size: 14px;
+            line-height: 26px;
+            margin: 0;
+            text-align: right;
+        }
+
+        .html {
+            -webkit-text-size-adjust: 100%;
+            -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+        }
+
+        .html {
+            direction: rtl;
+            overflow-x: hidden;
+            box-shadow: none !important;
+            -webkit-font-smoothing: antialiased;
+        }
+
+        :root {
+            --animate-duration: 1s;
+        }
+
+        @media (prefers-reduced-motion: no-preference) {
+            :root {
+                scroll-behavior: auto;
+            }
+        }
+
+        .border-ofn {
+            border: 0 !important;
+        }
+
+        .m-6ji {
+            margin: 0 !important;
+        }
+
+        section.section-j6v {
+            background: #f7f7f7;
+            border-top: 5px solid #f1f1f1;
+            margin: 30px 0;
+            padding: 50px 0;
+        }
+
+        #res-i5c {
+
+            background-color: #f3f3f3;
+            
+            background-position: 100% 100%; 
+            /* background-repeat: no-repeat; */
+            position: relative;
+            box-shadow: 11px 2px 26px 3px darkgrey;
+            /* border-radius: 64px;*/
+        }
+
+        *,
+        :before,
+        :after {
+            box-sizing: border-box;
+        }
+
+        #res-i5c:before {
+            content: "";
+            display: block;
+            height: 12px;
+            left: 0;
+            max-width: 465px;
+            position: absolute;
+            top: 0;
+            width: 50%;
+            background-color: #0088CC;
+        }
+
+        :selection {
+            color: #FFF;
+            background: #0088CC;
+        }
+
+        .h2 {
+            margin-bottom: .5rem;
+            margin-top: 0;
+        }
+
+        .h2 {
+            font-weight: 500;
+            line-height: 1.2;
+            margin-bottom: .5rem;
+        }
+
+        .h2 {
+            font-size: 2rem;
+        }
+
+        .h2 {
+            color: #212529;
+            font-weight: 200;
+            letter-spacing: -.05em;
+            margin: 0;
+            -webkit-font-smoothing: antialiased;
+        }
+
+        .h2 {
+            font-size: 2.2em;
+            font-weight: 300;
+            line-height: 42px;
+            margin: 0 0 32px 0;
+        }
+
+        a {
+            color: #007bff;
+            cursor: pointer;
             text-decoration: none;
         }
 
-        .box-iyc:hover {
+        a {
+            color: #c99e2d;
+        }
+
+        a {
+            text-decoration: none;
+        }
+
+        a {
+            color: #1b3c6a;
+        }
+
+        a {
+            outline: none !important;
+        }
+
+        a:hover {
+            color: #0056b3;
+            text-decoration: underline;
+        }
+
+        a:hover {
+            color: #0099e6;
+        }
+
+        a,
+        a:hover {
+            color: #1b3c6a;
+        }
+
+        a:not(.btn):hover {
+            text-decoration: none;
+        }
+
+        /* These were inline style tags. Uses id+class to override almost everything */
+        /* Section Styles */
+        /* Section Styles */
+        #res-i5c.section-j6v {
+            border: none;
+            padding: 20px;
+            border-radius: 10px;
+        }
+
+        .section-header {
+            display: flex;
+            justify-content: center;
+            max-width: 1562px;
+            padding-left: 20px;
+            padding-right: 20px;
+            margin-right: auto !important;
+            margin-left: auto !important;
+        }
+
+        .section-header-inner {
+            text-align: center !important;
+            line-height: 1.5;
+            font-size: 32px;
+            font-weight: 500;
+            margin: 30px 0;
+        }
+
+        .section-header-inner a {
+            color: #212529;
+            text-decoration: none;
+            position: relative;
+        }
+
+        .section-header-inner a::after {
+            content: '';
+            position: absolute;
+            width: 0;
+            height: 3px;
+            background-color: #007bff;
+            display: block;
+            bottom: -5px;
+            left: 50%;
+            transform: translateX(-50%);
+            transition: 0.5s;
+        }
+
+        .section-header-inner a:hover::after {
+            width: 90%;
+        }
+
+        /* Button Styles */
+        .section-btn-coll {
+            display: flex;
+            justify-content: center;
+            margin-top: 20px;
+        }
+
+        .section-btn-coll .section-button {
+            display: inline-block;
+            background-color: #007bff;
             color: #fff;
-            background: #c79d2d;
+            padding: 8px 16px;
+            text-decoration: none;
+            border-radius: 4px;
+            margin: 0 15px;
+            transition: background-color 0.3s ease;
+            font-size: large;
+        }
+
+        .section-btn-coll .section-button:hover {
+            background-color: #0056b3;
+        }
+
+        .section-btn-coll .section-button:first-of-type {
+            margin-left: 0;
+        }
+
+        .section-btn-coll .section-button:last-of-type {
+            margin-right: 0;
+        }
+
+        @media (max-width: 1562px) {
+            .section-header {
+                padding-left: 18px;
+                padding-right: 18px;
+            }
         }
     </style>
     <div class="contener">
@@ -369,8 +378,7 @@
                         <div class="row align-items-xl-center snipcss0-1-1-2">
                             <div class="col-9 snipcss0-2-2-6">
                                 <h2 class="font-weight-normal text-6 snipcss0-3-6-7" data-lfr-editable-id="card2"
-                                    data-lfr-editable-type="rich-text">كلمة العميد&nbsp;<br class="snipcss0-4-7-8">
-                                    <strong class="font-weight-extra-bold snipcss0-4-7-9">د. خالد بانافع </strong>
+                                    data-lfr-editable-type="rich-text">نبذة عن الجامعة&nbsp;<br class="snipcss0-4-7-8">
                                 </h2>
                             </div>
                         </div>
@@ -389,7 +397,6 @@
                                     المتداولة تحتوي مضامين مختلفة تشكل ثقافة الإنسان وتدير دفة حضارة المجتمعات. ففي ضوء هذا
                                     الادراك لأهمية الحاسوب وتقنية المعلومات فقد حرصت جامعة نجران منذ نشأتها على إطلاق برامج
                                     نوعية في مجالات التقنية باستحداث كلية علوم الحاسب ونظم المعلومات.</p>
-
                             </div>
                         </div>
                     </div>
@@ -432,4 +439,16 @@
             </div>
         </div>
     </div>
+    <section id="res-i5c" class="section-j6v">
+        <div class="section-header">
+            <div class="section-header-inner">
+                <a href="#">الاقسام</a>
+            </div>
+        </div>
+        <div class="section-btn-coll">
+            <a href="#" class="section-button">علوم الحاسوب</a>
+            <a href="#" class="section-button">تقنية المعلومات</a>
+            <a href="#" class="section-button">شبكات</a>
+        </div>
+    </section>
 @endsection
